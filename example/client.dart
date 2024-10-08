ff8480a1f5c23beb7f742ef5afd0463ffd42b269
NguyenVanDuong <nguyenvanduongtdh@gmail.com>
2024-10-08 13:17:02 +0100

    aaa

--- example/client.dart
+++ example/client.dart
@@ -2,13 +2,13 @@ import 'package:open62541/open62541.dart';
 
 Future<void> main() async {
   UAClient client = UAClient();
-  final rev = client.connect("opc.tcp://duong-Surface-Pro:4840/");
+  final rev = client.connect("opc.tcp://DESKTOP-MG0D01H:4840/");
   print("connect: $rev");
 
-  for (var i = 0; i < 1000; i++) {
+  for (var i = 0; i < 1; i++) {
     await Future.delayed(const Duration(milliseconds: 1));
-      await client.writeNodeIdAsync(UANodeId(1, "Availible"), "123".uaString());
-      print("read: ${await client.readNodeIdAsync(UANodeId(1, "Availible"))}");
+    // await client.writeNodeIdAsync(UANodeId(1, "Availible"), "123".uaString());
+    print("read: ${await client.readNodeIdAsync(UANodeId(1, "Availible"))}");
   }
   // client.disconnect();
 }
