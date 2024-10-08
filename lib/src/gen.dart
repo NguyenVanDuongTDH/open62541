6803a5ad6d4c74c8aebf30e930a204ff50862c66
NguyenVanDuong <nguyenvanduongtdh@gmail.com>
2024-10-08 16:37:11 +0700

    ok

--- lib/src/gen.dart
+++ lib/src/gen.dart
@@ -6,10 +6,10 @@ NativeLibrary get cOPC => NativeLibrary(DynamicLibrary.open(_getPathLib()));
 
 String _getPathLib() {
   if (Platform.isLinux) {
-    return "/home/duong/Desktop/open62541/open62541_1_3/open62541.so";
-    return "${Platform.script.toFilePath().replaceAll('/server', "")}/open62541_1_3/open62541.so";
+    return "/home/duong/Desktop/open62541/cpp/open62541.so";
+    return "${Platform.script.toFilePath().replaceAll('/server', "")}/cpp/open62541.so";
   } else if (Platform.isWindows) {
-    return "open62541_1_3/open62541.dll";
+    return "cpp/open62541.dll";
   } else if (Platform.isAndroid) {
     return "libopen62541.so";
   }
